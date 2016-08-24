625b44e445d6a08926f426879e40fc08799c6b45
nathanfaucett <nathanfaucett@gmail.com>
2016-08-24 10:20:14 -0500

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -36,19 +36,28 @@ impl Multiboot2 {
     }
 
     pub fn get_elf_sections_tag(&self) -> Option<&'static ElfSectionsTag> {
-        self.get_tag(9).map(|tag| unsafe{&*(tag as *const Tag as *const ElfSectionsTag)})
+        self.get_tag(9).map(|tag| unsafe{
+            &*(tag as *const Tag as *const ElfSectionsTag)
+        })
     }
 
     pub fn get_memory_map_tag(&self) -> Option<&'static MemoryMapTag> {
-        self.get_tag(6).map(|tag| unsafe{&*(tag as *const Tag as *const MemoryMapTag)})
+        self.get_tag(6).map(|tag| unsafe{
+            &*(tag as *const Tag as *const MemoryMapTag)
+        })
     }
 
     fn has_valid_end_tag(&self) -> bool {
-        const END_TAG: Tag = Tag{typ:0, size:8};
+        const END_TAG: Tag = Tag{
+            typ: 0,
+            size: 8,
+        };
 
         let self_ptr = self as *const _;
         let end_tag_addr = self_ptr as usize + (self.total_size - END_TAG.size) as usize;
-        let end_tag = unsafe{&*(end_tag_addr as *const Tag)};
+        let end_tag = unsafe{
+            &*(end_tag_addr as *const Tag)
+        };
 
         end_tag.typ == END_TAG.typ && end_tag.size == END_TAG.size
     }
@@ -58,7 +67,9 @@ impl Multiboot2 {
     }
 
     fn get_tags(&self) -> TagIter {
-        TagIter{ current: &self.first_tag as *const _ }
+        TagIter {
+            current: &self.first_tag as *const _,
+        }
     }
 }
 
@@ -76,8 +87,11 @@ impl Iterator for TagIter {
     type Item = &'static Tag;
 
     fn next(&mut self) -> Option<&'static Tag> {
-        match unsafe{&*self.current} {
-            &Tag{typ:0, size:8} => None,
+        match unsafe { &*self.current } {
+            &Tag{
+                typ: 0,
+                size: 8,
+            } => None,
             tag => {
                 let mut tag_addr = self.current as usize;
                 tag_addr += tag.size as usize;
